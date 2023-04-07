a7bb273946f2ad13fe6fbfcf959552b575efed21
SeryiBaran <seryibaranminepy@yandex.ru>
2023-04-07 22:11:06 +0300

    add operations && remove example of code (too long)

--- gen.js
+++ gen.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 
 const MIN_NUMBER = 0;
-const MAX_NUMBER = 4;
+const MAX_NUMBER = 10;
+const SIGNS = ["+", "-", "/", "*"];
 
 let calcCode = "";
 
@@ -13,8 +14,12 @@ using namespace std;
 
 int main() {
 
+  string operation;
   int a;
   int b;
+
+  cout << "Enter an operation: ";
+  cin >> operation;
 
   cout << "Enter an a: ";
   cin >> a;
@@ -23,18 +28,29 @@ int main() {
   cin >> b;
 `;
 
-for (let i = MIN_NUMBER; i < MAX_NUMBER + 1; i++) {
-  for (let j = MIN_NUMBER; j < MAX_NUMBER + 1; j++) {
-    calcCode += `
-  ${
-    i === MIN_NUMBER && j === MIN_NUMBER ? "" : "else "
-  }if (a == ${i} && b == ${j}) { cout << ${i + j} << endl; }`;
+SIGNS.forEach((sign) => {
+  for (let i = MIN_NUMBER; i < MAX_NUMBER + 1; i++) {
+    for (let j = MIN_NUMBER; j < MAX_NUMBER + 1; j++) {
+      calcCode += `
+    ${
+      i === MIN_NUMBER && j === MIN_NUMBER ? "" : "else "
+    }if (a == ${i} && operation == "${sign}" && b == ${j}) { ${
+        sign === "/" && (i === 0 || j === 0)
+          ? 'cout << "Zero division! Exit!" << endl; return 1;'
+          : `cout << ${i} ${sign} ${j} << endl;`
+      } }`;
+    }
   }
-}
+});
 
 calcCode += `
+  else if (${SIGNS.map(sign => `operation != "${sign}"`).join(" && ")}) {
+    cout << "Unknown operation (allowed - '${SIGNS.join("', '")}')" << endl;
+    return 1;
+  }
   else {
     cout << "Out of range (Min - ${MIN_NUMBER}, max - ${MAX_NUMBER})" << endl;
+    return 1;
   }
   return 0;
 }
