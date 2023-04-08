9d335d10f52911e9e7af143244392553b286f1dc
SeryiBaran <seryibaranminepy@yandex.ru>
2023-04-08 11:40:50 +0300

    ava tests

--- src/test/get.test.ts
+++ src/test/get.test.ts
@@ -1,7 +1,7 @@
-import { expect, test } from "vitest";
-import { generateCalcCode } from "../utils";
+import test from 'ava';
+import { generateCalcCode } from '../utils';
 
-test("Generate code", () => {
-  const result = generateCalcCode(0, 4, ["+", "-", "/", "*"]);
-  expect(result).toMatchSnapshot();
+test('Generate code', t => {
+  const result = generateCalcCode(0, 4, ['+', '-', '/', '*']);
+  t.snapshot(result);
 });
